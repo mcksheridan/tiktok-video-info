dee75097f997f18440d48cc654e5e69a11fd3ded
Sheridan McKisick <mcksheridan@gmail.com>
2021-03-07 11:51:30 -0600

    Added an async function inside of getVideoData to wrap all relavent functions together inside of the async.waterfall method. To test functionality, I created a getRedirectedUrl function based off of the previous content of the getVideoData function, and then I created a sendRedirectedUrlToBrowser function to ensure that the data showed up properly.

--- getVideoData.js
+++ getVideoData.js
@@ -1,11 +1,19 @@
 import https from 'follow-redirects/https';
+import async from 'async';
 
 const getVideoData = (req, res) => {
   const userInputVideoUrl = req.query.url;
-  https.get(userInputVideoUrl, (response) => {
-    const redirectedUrl = response.responseUrl;
-    res.send(redirectedUrl);
-  });
+  async.waterfall([
+    function getRedirectedUrl(callback) {
+      https.get(userInputVideoUrl, (response) => {
+        const redirectedUrl = response.responseUrl;
+        callback(null, redirectedUrl);
+      });
+    },
+    function sendRedirectedUrlToBrowser(redirectedUrl) {
+      res.send(redirectedUrl);
+    },
+  ]);
 };
 
 export { getVideoData as default };
