6fc7651d1a06b242a2cdc5417ecddff0a6ee49a3
Sheridan McKisick <mcksheridan@gmail.com>
2021-03-07 22:20:10 -0600

    Had no need of CSS file so removed it along with the path import

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 // eslint-disablenext-line import/no-extraneous-dependencies
 import express from 'express';
-import path from 'path';
 import { getVideoData } from './getVideoData';
 
 const app = express();
 const PORT = 3000;
-
-app.use(express.static(path.join(__dirname, 'src/css')));
 
 app.get('/', getVideoData);
 
